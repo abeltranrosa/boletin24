16e41f754ee2b89af663c3b94b4252754fcb18c3
antonio <antonio@192.168.0.11>
2017-06-05 12:08:54 +0200

    Clase ventana2 boletin 24

--- src/boletin24/Ventana2.java
+++ src/boletin24/Ventana2.java
@@ -1,19 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package boletin24;
 
-/**
- *
- * @author klemavilanova
- */
+
 public class Ventana2 extends javax.swing.JFrame {
 
-    /**
-     * Creates new form Ventana2
-     */
+   
     public Ventana2() {
         initComponents();
     }
@@ -198,10 +189,8 @@ public class Ventana2 extends javax.swing.JFrame {
         //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new Ventana2().setVisible(true);
-            }
+        java.awt.EventQueue.invokeLater(() -> {
+            new Ventana2().setVisible(true);
         });
     }
 
